583e8accaeb9b5427c7d94e82316b8c0cee901aa
Roman Khrabatyn <romankhr@yahoo.com>
2021-01-07 15:42:50 +0200

    Updated  FirstWebTests by using DataProvider

--- couse-QA-2020/src/test/java/com/course_qa2020/course/FirstWebTests.java
+++ couse-QA-2020/src/test/java/com/course_qa2020/course/FirstWebTests.java
@@ -12,7 +12,7 @@ import org.testng.annotations.Test;
 
 public class FirstWebTests extends BaseTest {
     private String acceptCookiesButtonLocator = "//button[@class=\"ui-button-primary ui-cookie-accept-all-medium-large\"]";
-//    private String searchTerm="London";
+    private String searchTerm="London";
     private HomePage homePage;
     private LocationLondonPage locationLondonPage;
     private PriceMinPage priceMinPage;
@@ -61,71 +61,71 @@ public class FirstWebTests extends BaseTest {
 
     //   -----------------------------Test 2-----------------------------
 
-//    @Test(groups = "main", suiteName = "ui")
-//    public void minPriceTest() throws Exception {
-//    priceMinPage=new PriceMinPage(driver);
-//        //Given user navigates to the webpage www.zoopla.co.uk
-//
-//        //When User inserts location "London" in the location field and Chooses Minimum price in the field "Min price" and pushes the button "Search"
-//         priceMinPage=homePage.setSearchLocationTerm(searchTerm).setMinPrice().clickOnSearchButton(PriceMinPage.class);
-//
-//        //Then user is redirected to the searched page. The information about the properties of chosen location with minimum price should appeared
-//           Assert.assertTrue(priceMinPage.isTitleCorrect(), "Title is not correct!!!");
-//    }
-//
-//    //   -----------------------------Test 3-----------------------------
-//
-//    @Test(groups = "main", suiteName = "ui")
-//    public void maxPriceTest() throws Exception {
-//
-//        //Given user navigates to the webpage www.zoopla.co.uk
-//        priceMaxPage=new PriceMaxPage(driver);
-//        //When User inserts location "London" in the location field and Chooses Maximum price in the field "Max price" and pushes the button "Search"
-//        priceMaxPage=homePage.setSearchLocationTerm(searchTerm).setMaxPrice().clickOnSearchButton(PriceMaxPage.class);
-//
-//        //Then user is redirected to the searched page. The information about the properties of chosen location with maximum price should appeared
-//        Assert.assertTrue(priceMaxPage.isTitleCorrect(), "Title is not correct!!!");
-//    }
-//
-//    //   -----------------------------Test 4 -----------------------------
-//
-//    @Test(groups = "main", suiteName = "ui")
-//    public void propertyTest() throws Exception {
-//        propertyTypePage=new PropertyTypePage(driver);
-//
-//        //Given user navigates to the webpage www.zoopla.co.uk
-//
-//        //When User inserts location "London" in the location field and chooses the type of property in "Property type" and pushes the button "Search"
-//        propertyTypePage=homePage.setSearchLocationTerm(searchTerm)
-//                .setPropertyType()
-//                .clickOnSearchButton(PropertyTypePage.class);
-//
-//        //Then user is redirected to the searched page. The information about the properties of chosen location and type should appeared
-//        Assert.assertTrue(propertyTypePage.isTitleCorrect(), "Title is not correct!!!");
-//    }
-//
-//    //   -----------------------------Test 5 -----------------------------
-//
-//    @Test(groups = "main", suiteName = "ui")
-//    public void numbersOfBedroomsTest() throws Exception {
-//        advanceSearchPage = new AdvanceSearchPage(driver);
-//        //Given user navigates to the webpage www.zoopla.co.uk
-//
-//        //When User inserts location "London" in the location field and chooses the type of property in "Property type"
-//        //and Chooses Maximum price in the field "Max price"  and Chooses Minimum price in the field "Min price"
-//        // and Chooses Distance Radius and number of Bedrooms and pushes the button "Search"
-//
-//        advanceSearchPage=homePage
-//                .setSearchAdvance()
-//                .setPropertyType()
-//                .setMinPrice()
-//                .setSearchLocationTerm(searchTerm)
-//                .setMaxPrice().setSearchByBedrooms()
-//                .setSortOptions()
-//                .setSearchRadius()
-//                .clickOnSearchButton(AdvanceSearchPage.class);
-//
-//        //Then user is redirected to the searched page. The information about the chosen properties should appeared
-//        Assert.assertTrue(advanceSearchPage.isTitleCorrect(), "Title is not correct!!!");
-//    }
+    @Test(groups = "main", suiteName = "ui")
+    public void minPriceTest() throws Exception {
+    priceMinPage=new PriceMinPage(driver);
+        //Given user navigates to the webpage www.zoopla.co.uk
+
+        //When User inserts location "London" in the location field and Chooses Minimum price in the field "Min price" and pushes the button "Search"
+         priceMinPage=homePage.setSearchLocationTerm(searchTerm).setMinPrice().clickOnSearchButton(PriceMinPage.class);
+
+        //Then user is redirected to the searched page. The information about the properties of chosen location with minimum price should appeared
+           Assert.assertTrue(priceMinPage.isTitleCorrect(), "Title is not correct!!!");
+    }
+
+    //   -----------------------------Test 3-----------------------------
+
+    @Test(groups = "main", suiteName = "ui")
+    public void maxPriceTest() throws Exception {
+
+        //Given user navigates to the webpage www.zoopla.co.uk
+        priceMaxPage=new PriceMaxPage(driver);
+        //When User inserts location "London" in the location field and Chooses Maximum price in the field "Max price" and pushes the button "Search"
+        priceMaxPage=homePage.setSearchLocationTerm(searchTerm).setMaxPrice().clickOnSearchButton(PriceMaxPage.class);
+
+        //Then user is redirected to the searched page. The information about the properties of chosen location with maximum price should appeared
+        Assert.assertTrue(priceMaxPage.isTitleCorrect(), "Title is not correct!!!");
+    }
+
+    //   -----------------------------Test 4 -----------------------------
+
+    @Test(groups = "main", suiteName = "ui")
+    public void propertyTest() throws Exception {
+        propertyTypePage=new PropertyTypePage(driver);
+
+        //Given user navigates to the webpage www.zoopla.co.uk
+
+        //When User inserts location "London" in the location field and chooses the type of property in "Property type" and pushes the button "Search"
+        propertyTypePage=homePage.setSearchLocationTerm(searchTerm)
+                .setPropertyType()
+                .clickOnSearchButton(PropertyTypePage.class);
+
+        //Then user is redirected to the searched page. The information about the properties of chosen location and type should appeared
+        Assert.assertTrue(propertyTypePage.isTitleCorrect(), "Title is not correct!!!");
+    }
+
+    //   -----------------------------Test 5 -----------------------------
+
+    @Test(groups = "main", suiteName = "ui")
+    public void numbersOfBedroomsTest() throws Exception {
+        advanceSearchPage = new AdvanceSearchPage(driver);
+        //Given user navigates to the webpage www.zoopla.co.uk
+
+        //When User inserts location "London" in the location field and chooses the type of property in "Property type"
+        //and Chooses Maximum price in the field "Max price"  and Chooses Minimum price in the field "Min price"
+        // and Chooses Distance Radius and number of Bedrooms and pushes the button "Search"
+
+        advanceSearchPage=homePage
+                .setSearchAdvance()
+                .setPropertyType()
+                .setMinPrice()
+                .setSearchLocationTerm(searchTerm)
+                .setMaxPrice().setSearchByBedrooms()
+                .setSortOptions()
+                .setSearchRadius()
+                .clickOnSearchButton(AdvanceSearchPage.class);
+
+        //Then user is redirected to the searched page. The information about the chosen properties should appeared
+        Assert.assertTrue(advanceSearchPage.isTitleCorrect(), "Title is not correct!!!");
+    }
 }
