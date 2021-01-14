3e3ad8542fade3be6623f365e4c7f28f70028f27
Oleksandr Perepelytsya <o.perepelytsya@gmail.com>
2021-01-14 10:11:17 +0000

    Update PriceMaxPage.java

--- couse-QA-2020/src/main/java/com/course_qa2020/pages/PriceMaxPage.java
+++ couse-QA-2020/src/main/java/com/course_qa2020/pages/PriceMaxPage.java
@@ -4,6 +4,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.remote.RemoteWebDriver;
 
 public class PriceMaxPage extends Page{
+    //TODO same as previously about naming and using capitals
     private String priceMaxLocator="//div[@class='css-ijwzpf-ButtonWrapper e1tnwg465']//p[contains(text(),'£500k max')]";
     private String LOCATION_PAGE_TITLE_MAX="£500k max";
 
